9db145e68e8ad2b87aee3032360427dd434e0e95
GalteXx <maximkors@gmail.com>
2024-03-04 15:01:43 +0300

    Commands WIP

--- Paster/Model/Phrase.cs
+++ Paster/Model/Phrase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Paster.Model
 {
@@ -10,14 +11,25 @@ namespace Paster.Model
     {
         private readonly string name;
         private readonly string text;
-
-        public Phrase(string name, string text)
-        {
-            this.name = name;
-            this.text = text;
-        }
-
+        
+        public ICommand InputSelf { get; }
         public string Name { get => name; }
         public string Text { get => text; }
+
+
+        public Phrase(string _name, string _text)
+        {
+            name = _name;
+            text = _text;
+            InputSelf = new RoutedCommand("InputSelf", typeof(Phrase));
+            
+        }
+
+        private void ExecuteTypeSelf()
+        {
+            OutputManager.Type(text);
+        }
+
+
     }
 }
